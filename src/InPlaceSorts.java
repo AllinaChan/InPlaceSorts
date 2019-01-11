a0a7c4f1c86aa1453b0ed133699b72973e90cd7d
BT_1N3_30 <zchen5143@bths.edu>
2019-01-11 14:09:19 -0500

    BUBBLE SORT DONE!

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -75,7 +75,7 @@ public class InPlaceSorts {
         for(int i =0; i <arr.length; i++)
         {
             if(i+1!=arr.length) {
-                if (arr[i].compareTo(arr[i + 1])<0) {
+                if (arr[i].compareTo(arr[i + 1])>0) {
                     result = false;
                     break;
                 }
