c73e554d48347a265da46600fef4717975457e46
BT_1N3_30 <zchen5143@bths.edu>
2019-01-15 14:50:31 -0500

    sdgjkf

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -110,13 +110,12 @@ public class InPlaceSorts {
     public static void insertionSort(int[] arr)
     {
         int currentPos=1;
-
         for(int i=1; i<arr.length; i++)
         {
             for(int j=i; j>0; j--)
             {
-                if () {
-                    InPlaceSorts.swap(arr, j-1,j );
+                if (arr[j]>arr[j-1]) {
+                    InPlaceSorts.swap(arr, j,j-1 );
                 }else{
                     break;
                 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,9 @@
 public class Runner {
         public static void main(String[] args)
         {
-            String[] arr= InPlaceSorts.randomStringArr(5, 5);
-
+            String[] stringArr= InPlaceSorts.randomStringArr(5, 5);
+            int[] intArr= InPlaceSorts.randIntArr(5);
+            double[] doubleArr= InPlaceSorts.randDoubleArr(5);
 
             System.out.println("Before: ");
             System.out.println(InPlaceSorts.printArr(arr));
@@ -10,7 +11,7 @@ public class Runner {
             long time= System.nanoTime();
            //InPlaceSorts.bubbleSort(arr);
             //InPlaceSorts.selectionSort(arr);
-            InPlaceSorts.insertionSort(arr);
+            InPlaceSorts.insertionSort(intArr);
             System.out.println("After: ");
             System.out.println(InPlaceSorts.printArr(arr));
             time=System.nanoTime()-time;
