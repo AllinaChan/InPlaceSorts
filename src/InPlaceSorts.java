d02a1021398a0c5a443288e837408ec2c32b3443
asdasd <asdasdas>
2019-02-01 14:34:56 -0500

    LOL

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -233,6 +233,20 @@ public class InPlaceSorts {
         return newMerge;
 
     }
+    public void mergeSort(int[] arr){
+        int n=arr.length;
+        int[] temp=new int[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
+
+    public void mergeSortHelper(int[] arr, int left, int right,int[]temp){
+        if(left<right){
+            int mid=(left+right)/2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr,mid+1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
 
 
 
